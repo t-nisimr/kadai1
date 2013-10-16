fb9c7fdc57f9c22a3840574081d6b7712bfc8368
m-ishino <m-ishino@m-ishino-laptop.(none)>
2013-10-16 12:49:57 +0900

    First Commit

---
+++ hello-trema.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+#
+# Hello Trema
+#  
+# Copyright (c) 2013, Masanori Ishino.
+# All rights reserved.
+# 
+# $Id: hello-trema.rb 2013/09/18 16:51:46 m-ishino Exp $
+#
+
+class HelloTrema < Controller
+  def start
+    puts "Hi! from " + name
+  end
+end
+
+# Local variables:
+# mode: Ruby
+# coding: utf-8
+# indent-tabs-mode: nil
+# End:
